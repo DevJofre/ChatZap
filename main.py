c79589483c5cfe55250b630c65ceafd6a1100df6
Jofre Tomas <jofretomas9@gmail.com>
2024-10-26 16:15:28 -0300

    construção do chat

--- main.py
+++ main.py
@@ -4,24 +4,39 @@ import flet as ft
 def main(page):
 
     title = ft.Text("ChatZap")
-    button_init = ft.ElevatedButton("Iniciar Chat")
 
-    send_message = ft.TextField(label="Digite sua mensagem aqui...")
-    button_send = ft.ElevatedButton("Enviar")
+    def send_message(event):
+
+        name_user = box_chat.value
+        text_dig = box_message.value
+        text = ft.Text(f"{name_user}: {text_dig}")
+        chat.controls.append(text)
+
+        page.update()
+
+    box_message = ft.TextField(
+        label="Digite sua mensagem aqui...", on_submit=send_message)
+
+    button_send = ft.ElevatedButton(
+        "Enviar", on_click=send_message)
+
+    chat = ft.Column([])
+    row_send = ft.Row([box_message, button_send])
 
     def on_chat(evento):
         popup.open = False
         page.remove(title)
         page.remove(button_init)
 
-        page.add(send_message)
-        page.add(button_send)
+        page.add(chat)
+        page.add(row_send)
 
         page.update()
 
     title_popup = ft.Text("Bem Vindo ao ChatZap.")
-    box_chat = ft.TextField(label="Digite seu nome.")
-    button_popup = ft.ElevatedButton("Entra no Chat", on_click=on_chat)
+    box_chat = ft.TextField(label="Digite seu nome.", on_submit=on_chat)
+    button_popup = ft.ElevatedButton(
+        "Entra no Chat", on_click=on_chat)
 
     popup = ft.AlertDialog(
         title=title_popup, content=box_chat, actions=[button_popup])
@@ -31,7 +46,8 @@ def main(page):
         popup.open = True
         page.update()
 
-    button_init.on_click = open_popup
+    button_init = ft.ElevatedButton(
+        "Iniciar Chat", on_click=open_popup)
 
     page.add(title)
     page.add(button_init)
